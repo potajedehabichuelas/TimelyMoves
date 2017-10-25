056679ff5dff30ded2495ccd895d9f8260751554
Daniel Bolivar <dbolivarh@gmail.com>
2017-10-25 15:05:46 +0200

    Added Mockup for TrackerTableview Interface

---
+++ TimelyMoves/ViewControllers/TrackerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrackerTableViewController.h
+//  TimelyMoves
+//
+//  Created by Daniel Bolivar herrera on 25/10/2017.
+//  Copyright © 2017 Daniel Bolivar herrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrackerTableViewController : UITableViewController
+
+@end
